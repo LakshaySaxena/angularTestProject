794934ebef95d18cde809f60a1dab434cc3c4a80
LakshaySaxena <lakshaysaxena8@gmail.com>
2022-06-02 23:45:12 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public color = 'Star Lord';
+  public hero = 'Star Lord';
 }
